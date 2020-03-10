3bad3b381aeeee05a3fe1d46590375303945ad4d
monicuta2008 <monicuta2008@yahoo.com>
2020-03-10 13:03:25 +0200

    procentaje masini - dashboard - JS + CSS

--- js/code.js
+++ js/code.js
@@ -13,3 +13,57 @@
         menu.classList.toggle('show-menu');
     }
  };
+
+
+/*codul pentru pie de pe dashboard */
+/*invatat de pe https://css-tricks.com/how-to-make-charts-with-svg/ */
+var pie = document.querySelector('.pie');
+var buttons = document.querySelector('.masini-menu-2019');
+var circumferinta = 628;
+
+var masini = {
+  Dacia: 60,
+  Mercedes : 5,
+  Volvo: 9,
+  Tesla: 1,
+  Seat: 15,
+  Fiat: 12
+};
+
+for(masina in masini){ //adaugam pentru fiecare element un buton in meniu
+  var buton_masina = document.createElement('div');
+  buton_masina.setAttribute('data', masina);
+  buton_masina.innerText = masina;
+  buttons.appendChild(buton_masina);
+}
+
+buttons.addEventListener('click', function(e){
+    if(e.target != e.currentTarget){
+        var el = e.target;
+        var masina = el.getAttribute('data');
+        setPieChart(masina);
+        setActiveClass(el);
+    }
+    e.stopPropagation();
+});
+
+var setPieChart = function(nume_masina){
+    var number = masini[nume_masina];
+    var fixedNumber = (number*circumferinta)/100;
+    var result = fixedNumber + ' ' + circumferinta;
+    pie.style.strokeDasharray = result;
+}
+
+var setActiveClass = function(el) {
+  for(var i = 0; i < buttons.children.length; i++) {
+    buttons.children[i].classList.remove("active");
+    el.classList.add("active");
+  }
+}
+
+//setarile default
+setPieChart('Dacia');
+setActiveClass(buttons.children[0]);
+
+
+ /*end --- codul pentru pie de pe dashboard */
