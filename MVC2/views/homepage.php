5cbbe5875c482e8393a8dacbe09fca4abc8c9def
monicuta2008 <monicuta2008@yahoo.com>
2020-06-08 10:01:03 +0300

    adauga meta si fixeaza alte warning-uri din css

--- MVC2/views/homepage.php
+++ MVC2/views/homepage.php
@@ -5,6 +5,7 @@
         <meta name="Author" content="Macovei Monica-Ioana">
         <meta name="Author" content="Luca Nicoleta">
         <meta name="Description" content="Instrument Web de vizualizare adecvata si de comparare multi-criteriala a datelor publice privind parcul auto din Romania pe ultimii 5 ani">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css?family=Stardos+Stencil:400,700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Quicksand:400,700&display=swap" rel="stylesheet">
 
